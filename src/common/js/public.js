7d143c04fe5c8ea53d5d9272cf287ff04b85da98
zhangzhenchang <591781589@qq.com>
2019-06-06 16:41:09 +0800

    public 改测试地址

--- src/common/js/public.js
+++ src/common/js/public.js
@@ -1,7 +1,7 @@
-let baseURL="https://wallet-api.launchain.cn";
-let loginPlatform="https://login.datajs.com.cn";
-let exchangePlatform="https://exchange.datajs.com.cn";
-let transferPlatform="https://transfer.datajs.com.cn";
-let searchPlatform="https://search.datajs.com.cn";
+let baseURL="https://wallet-api-test.launchain.org";
+let loginPlatform="https://login-test.datajs.com.cn";
+let exchangePlatform="https://exchange-test.datajs.com.cn";
+let transferPlatform="https://transfer-test.datajs.com.cn";
+let searchPlatform="https://search-test.datajs.com.cn";
 export {baseURL,loginPlatform,exchangePlatform,transferPlatform,searchPlatform}
 
